d27081f548ff6d2e0e465a0679de4f18f235cf24
Lois KOUNINEF <lkouninef@gmail.com>
2023-02-14 11:32:13 +0100

    clear console on start

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ import Messages from "./app/messages.js";
 
 export const sleep = (ms = 1000) => new Promise((r) => setTimeout(r, ms));
 
+console.clear();
+
 const messages = new Messages;
 const inputs = new Inputs;
 await messages.welcome()
 await inputs.askName();
 await inputs.getDifficulty();
 await inputs.getEnemiesNumber();
-
-console.log(inputs.enemiesNumber)
