ad04d63a31821a694a217e8d8268ad8e429ab0d1
Lois KOUNINEF <lkouninef@gmail.com>
2023-02-14 12:22:12 +0100

    players classes

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 #!/usr/bin/env node
-import Inputs from "./app/inputs/settings-inputs.js";
-import Messages from "./app/messages/default-messages.js";
+import SettingsInputs from "./app/inputs/settings-inputs.js";
+import DefaultMessages from "./app/messages/default-messages.js";
 
 console.clear();
 
-const messages = new Messages;
-const inputs = new Inputs;
+const messages = new DefaultMessages;
+const inputs = new SettingsInputs;
 await messages.welcome()
 await inputs.askName();
 await inputs.getDifficulty();
